2e9057bd4bf5aadc2d6bb8e0ceca105cc925a845
clopezr9 <clopezr9@eafit.edu.co>
2021-08-23 00:36:11 -0500

    ready19

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ import sys
 import http.client
 import threading
 import time
+from requests import get
 
 if len(sys.argv) != 3:
 	print ("Correct usage: script, IP address, port number")
@@ -9,6 +10,8 @@ if len(sys.argv) != 3:
 
 ip_address = str(sys.argv[1])
 port = int(sys.argv[2])
+
+my_ip = get('https://api.ipify.org').text
 
 conn = http.client.HTTPConnection(ip_address, port)
 
@@ -28,6 +31,6 @@ while True:
     msg = input()
 
     if len(msg) != 0:
-        msg=  ip_address  + "->" + msg
+        msg=  my_ip  + "->" + msg
         conn.request("POST", "/", msg.encode())
         rsp  = conn.getresponse()
