0e2f92e594fc80537967cff9f024d25e9ad57acc
clopezr9 <clopezr9@eafit.edu.co>
2021-08-23 00:11:26 -0500

    ready15?

--- Client.py
+++ Client.py
@@ -5,16 +5,18 @@ import threading
 if len(sys.argv) != 3:
 	print ("Correct usage: script, IP address, port number")
 	exit()
+
 ip_address = str(sys.argv[1])
 port = int(sys.argv[2])
 
 conn = http.client.HTTPConnection(ip_address, port)
 
 def get_messages():
-    conn.request("POST", "/get", ip_address.encode())
-    rsp = conn.getresponse()
-    d = rsp.read().decode()
-    print(d)
+    while True:
+        conn.request("POST", "/get", ip_address.encode())
+        rsp = conn.getresponse()
+        d = rsp.read().decode()
+        print(d)
 
 thread = threading.Thread(target=get_messages, daemon=True)
 thread.start()
